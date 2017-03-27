fd715ce817aac66aab90b17b6f72e934e7d22510
Erica Winberry <elaewin@gmail.com>
2017-03-27 11:50:36 -0700

    Update Contents.swift

--- Swift/UIResponderExtensions.playground/Contents.swift
+++ Swift/UIResponderExtensions.playground/Contents.swift
@@ -2,11 +2,17 @@
 
 import UIKit
 
-// Create an string identifier on all classes that inherit from UIResponder
+// Two ways to create an string identifier on all classes that inherit from UIResponder - use ONE OR THE OTHER, not both.
 
 extension UIResponder {
     class func identifier() -> String {
         return String(describing: self)
     }
+    
+    static var identifier : String {
+        return String(describing: self)
+    }
 }
+
+
 
